9532db96022b1cc1b53c6d3e68980012e6b702d6
magi1001 <81128297+magi1001@users.noreply.github.com>
2021-04-10 18:05:19 +0900

    Add files via upload

---
+++ Exercise6_5.java
@@ -0,0 +1,17 @@
+package exercise6;
+class PlayingCard{
+	int kind;
+	int num;
+	
+	static int width;
+	static int height;
+	
+	PlayingCard(int k , int n){
+		kind = k;
+		num = n;
+	}
+	
+}
+public class Exercise6_5 {
+
+}
